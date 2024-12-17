ba6be7dc6fd9297666e5bc287911a717cfa7df45
Sihun Kim <skim585@asu.edu>
2024-12-17 12:30:49 -0700

    feat(layouts/): update layout in mobile screen
    add content for mobile nav.

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="relative lg:flex">
-    <HeaderVertical bg-color="dark-3">
+    <HeaderVertical bg-color="dark-3" drawer-btn-color="warning">
       <template #content>
         <div class="px-3xs">
           <h2 class="text-center text-warning mb-sm">Book Search</h2>
@@ -14,8 +14,21 @@
           </div>
         </div>
       </template>
+      <template #mobile-content>
+        <div class="px-3xs">
+          <h2 class="text-center text-warning mb-sm">Book Search</h2>
+          <div>
+            <RouterLink to="/">
+              <div class="text-center text-light-3 text-lg mb-sm">HOME</div>
+            </RouterLink>
+            <RouterLink to="/about">
+              <div class="text-center text-light-3 text-lg">ABOUT</div>
+            </RouterLink>
+          </div>
+        </div>
+      </template>
     </HeaderVertical>
-    <div class="w-full bg-dark-1 relative">
+    <div class="w-full bg-dark-1 relative min-h-screen">
       <div class="container px-2xs lg:px-xs mx-auto">
         <slot />
       </div>
